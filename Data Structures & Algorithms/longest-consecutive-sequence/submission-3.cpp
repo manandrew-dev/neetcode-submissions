f833c17bbbb194e89c35df0a2ad84a621a855f28
Andrew <103300627+manandrew-dev@users.noreply.github.com>
2026-05-08 15:34:06 -0700

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+class Solution {
+ public:
+  int longestConsecutive(vector<int>& nums) {
+    unordered_set<int> hashednums(nums.begin(), nums.end());
+    int longest = 0;
+    int length = 0;
+    int cur = 0;
+    for (int num : hashednums){
+      if (!hashednums.count(num-1)){
+        length = 0;
+        cur = num;
+        while (hashednums.count(cur)){
+          length++;
+          if (length>longest){
+            longest = length;
+          }
+          cur++;
+        }
+      }
+    }
+    return longest;
+  }
+};
